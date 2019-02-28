75409006b730d87a1861d178c3dc31b5db04f7f9
yangyuxi <yuxiyang.yang@qunar.com>
2019-02-28 11:47:57 +0800

    feat:弃掉了qunar的一些依赖

--- qapm/src/main/java/com/mqunar/qapm/QAPMConstant.java
+++ qapm/src/main/java/com/mqunar/qapm/QAPMConstant.java
@@ -5,8 +5,6 @@ package com.mqunar.qapm;
  */
 public class QAPMConstant {
 
-    // QAPM 自己上传路径，通过排除自己的上传路径来排除ANDROID_MONITOR监控请求数据
-    public static String t = "http://l-wap6.wap.beta.cn6.qunar.com:9088/api/log/androidPerformanceLog";
 
     // id 相关， 只有pid为外部必须传入， 其他参数为可选择传入
     public static String pid = "";      // 去哪儿旅行客户端Id
@@ -29,14 +27,6 @@ public class QAPMConstant {
     public static final String THREAD_UPLOAD = "QAPM-Thread-upload";
     public static final String THREAD_STORAGE = "QAPM-Thread-storage";
 
-//    public static String pitcher = "http://front.pitcher.beta.qunar.com/pitcher-proxy";
-//    public static String t = "http://l-client3.wap.beta.cn6.qunar.com:9088/api/log/androidPerformanceLog";
-//    public static String t = "http://l-wap6.wap.beta.cn6.qunar.com:9088/api/log/androidPerformanceLog";
-//    public static String t = "http://l-wappubsuport4.wap.dev.cn0.qunar.com:9088/api/log/androidMonitorLog";
-
-//    日志服务地址 dev: http://l-wappubsuport4.wap.dev.cn0.qunar.com:9088/api/log/androidPerformanceLog
-//    beta: http://l-wap6.wap.beta.cn6.qunar.com:9088/api/log/androidPerformanceLog
-//    prod: http://mwhale.corp.qunar.com/api/log/androidPerformanceLog
 
     public static final float DEFAULT_DEVICE_REFRESH_RATE = 16.666667f;
     public static final int TIME_MILLIS_TO_NANO = 1000000;
