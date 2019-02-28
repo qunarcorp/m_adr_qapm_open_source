11a0f480dca5df8d457d84f6677ecb0d92a55be4
yangyuxi <yuxiyang.yang@qunar.com>
2019-02-28 17:14:27 +0800

    feat:test

--- app/src/main/java/com/qunar/moudle/MainActivity.java
+++ app/src/main/java/com/qunar/moudle/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int i = 0;
         setContentView(R.layout.activity_main);
     }
 }
